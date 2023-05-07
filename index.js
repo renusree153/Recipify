2467ec1161ca68c5d5b28fa27590c47cda433d4c
rrenusree <renusree@uw.edu>
2023-05-06 19:37:30 -0700

    put in reviews

--- index.js
+++ index.js
@@ -1,4 +1,24 @@
-"use strict"
+'use strict';
 (function() {
-
+    window.addEventListener("load", init);
+    function init() {
+        document.getElementById("showRevPadThai").addEventListener("click", showRev);
+        document.getElementById("goBackRecipes").addEventListener("click", showRev);
+        let map = {}
+    }
+    function showRev() {
+        let board = document.querySelector("main");
+        let padThai = document.getElementById("padThaiReview");
+        console.log(board);
+        console.log(padThai);
+        if (board.classList.contains("dontShow")) {
+            console.log("in if");
+            board.classList.remove("dontShow");
+            padThai.classList.add("dontShow");
+        } else {
+            console.log("in else");
+            board.classList.add("dontShow");
+            padThai.classList.remove("dontShow");
+        }   
+    }
 })();
