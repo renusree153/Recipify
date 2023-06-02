b8f86c82e534d1f590b07a7b62c55a745c1cda78
tkes <tkes@uw.edu>
2023-06-02 02:29:03 -0700

    addUser and checkUser made into post method

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const multer = require("multer");
 
 app.use(express.urlencoded({extended: true, }));
 app.use(express.json());
+app.use(multer().none());
 
 async function getDBConnection() {
     const db = await sqlite.open({
@@ -18,16 +19,41 @@ async function getDBConnection() {
     return db;
 }
 
-app.get("/checkUser", async (req, res) => {
+app.post("/checkUser", async (req, res) => {
     let db = await getDBConnection();
-    let username = req.query.username;
-    console.log(username);
-    let password = req.query.password;
-    console.log(password);
-    let query = ("SELECT * FROM users");
+    let user = req.body.username;
+    let pass = req.body.password;
+    let query = ("SELECT * FROM users WHERE username='" + user + "' AND password='" + pass + "';");
     let results = await db.all(query);
-    console.log(results);
+    db.close();
+    res.json(results);
+});
+
+app.post('/addUser', async (req, res) => {
+    let user = req.body.username;
+    let pass = req.body.password;
+    let db = await getDBConnection();
+    const query = "INSERT INTO users (username, password)" +
+      " VALUES (?, ?)";
+    db.run(query, [user, pass]);
+    let users = await db.all("SELECT * FROM users");
+    db.close();
+    res.json(users);
 })
+
+app.get('/log', async (req, res) => {
+    console.log('hi there');
+});
+
+app.post('/test', async (req, res) => {
+    let test = req.body.test;
+    let db = await getDBConnection();
+    let all = await db.all("SELECT * FROM cart");
+    db.close();
+    res.json(all);
+})
+
+
 app.use(express.static("public"));
 const PORT = process.env.PORT || 8000;
 app.listen(PORT);
