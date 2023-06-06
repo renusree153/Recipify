c579c81c415087a35057041b0e37643bc71141f2
rrenusree <renusree@uw.edu>
2023-06-06 07:27:32 -0700

    more UI

--- public/purchases.js
+++ public/purchases.js
@@ -1,12 +1,32 @@
+/**
+ * Name: Renusree Chittella & Theodore Sakamoto
+ * Date: June 6th 2023
+ * This is the purchases.js file which contains information
+ * about displaying the past purchases for a user to the website.
+ */
+
 'use strict';
 (function() {
-
   window.addEventListener('load', init);
 
+  /**
+   * This is our init function which calls the fillPurchases
+   * function in order to populate the website with all the past
+   * purchases a given user has made.
+   */
   function init() {
     fillPurchases(window.localStorage.getItem('user'));
   }
 
+  /**
+   * This is our fillPurchases function which makes a
+   * fetch request to the /getPurchases endpoint
+   * and retrieves the purchaseID, date of purchase,
+   * among other information for a given user.
+   * @param {name} name - name is the user's name
+   * which is passed in as a parameter to retrieve
+   * purchase history for them
+   */
   function fillPurchases(name) {
     let bodyData = new FormData();
     bodyData.append("name", name);
@@ -36,12 +56,39 @@
       });
   }
 
+  /**
+   * This is our appendChildren function which
+   * takes in the different elements on the page and appends
+   * them onto the main element in order for it to be displayed
+   * to the user.
+   * @param {newDiv} newDiv - this is the main element in which
+   * we will be appening the other elements onto
+   * @param {img} img - this is the image of the item that is
+   * purchased
+   * @param {itemDes} itemDes - this is a description of the item
+   * that is going to be shown with the purchase history
+   * @param {purchaseId} purchaseId - this is the ID of the purchase
+   * which is also going to be shown to the user
+   * @param {purchaseDate} purchaseDate - this is the date in which
+   * each purchase was made by the user
+   */
   function appendChildren(newDiv, img, itemDes, purchaseId, purchaseDate) {
     newDiv.appendChild(img);
     newDiv.appendChild(itemDes);
     newDiv.appendChild(purchaseId);
     newDiv.appendChild(purchaseDate);
   }
+
+  /**
+   * This is my statusCheck function which checks
+   * whether or not the response from the endpoint
+   * was successful or not.
+   * @param {response} response takes in response
+   * as a parameter which is the status code received
+   * from the fetch request and checks if it is valid or not
+   * @returns {response} response - returns a response
+   * object that shows whether the fetch was successful or not.
+   */
   async function statusCheck(response) {
     if (!response.ok) {
       throw new Error(await response.text());
@@ -49,11 +96,18 @@
     return response;
   }
 
+  /**
+   * This is my handle error function which takes in
+   * the error message receivd from the fetch call and
+   * append a paragraph onto the webpage stating that
+   * there was an error.
+   * @param {err} err  - takes in an error object
+   * that is retrived from the fetch call.
+   */
   function handleError(err) {
     let p1 = document.createElement("p");
     p1.textContent = "There has been an error retrieving your data. Please try again" +
       "Here is the error: " + err;
     document.getElementById("main").appendChild(p1);
   }
-
 })();
--- public/sign-up.js
+++ public/sign-up.js
@@ -1,9 +1,32 @@
+/**
+ * Name: Renusree Chittella & Theodore Sakamoto
+ * Date: June 6th 2023
+ * This is the sign-up.js file which contains information
+ * about verifying if a given user's credentials are in the
+ * database and adding a new user when the sign up button
+ * is chosen
+ */
+
 'use strict';
 (function() {
   window.addEventListener("load", init);
+
+  /**
+   * This is our init function which calls the addUser function
+   * when the submit button is clicked in order to add a new user
+   * to the database.
+   */
   function init() {
     document.getElementById("submitBtn").addEventListener("click", addUser);
   }
+
+  /**
+   * This is the addUser function which retrieves the username and
+   * password values from the text box and appends those values
+   * to the database given that the username does not already exist.
+   * It makes a fetch call to the /addUser endpoint in order to
+   * post the data there.
+   */
   function addUser() {
     let username = document.getElementById("username").value;
     let password = document.getElementById("password").value;
@@ -29,6 +52,17 @@
         handleError(err);
       });
   }
+
+  /**
+   * This is my statusCheck function which checks
+   * whether or not the response from the endpoint
+   * was successful or not.
+   * @param {response} response takes in response
+   * as a parameter which is the status code received
+   * from the fetch request and checks if it is valid or not
+   * @returns {response} response - returns a response
+   * object that shows whether the fetch was successful or not.
+   */
   async function statusCheck(response) {
     if (!response.ok) {
       throw new Error(await response.text());
@@ -36,6 +70,14 @@
     return response;
   }
 
+  /**
+   * This is my handle error function which takes in
+   * the error message receivd from the fetch call and
+   * append a paragraph onto the webpage stating that
+   * there was an error.
+   * @param {err} err  - takes in an error object
+   * that is retrived from the fetch call.
+   */
   function handleError(err) {
     let p1 = document.createElement("p");
     p1.textContent = "There has been an error retrieving your data. Please try again" +
